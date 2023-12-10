dc43d7d42f35b184dbe28215ad9c85acfdcc9ec6
weinianguo <134699796+weinianguo@users.noreply.github.com>
2023-12-10 11:46:07 +0800

    Update fundation_content.py

--- fundation_content.py
+++ fundation_content.py
@@ -50,4 +50,25 @@
             tf.keras.layers.Dense(10, activation='softmax')])
         model.load_weights(model_save_path)
         result=model.predict(x_predict)
+(4)图像处理博客的网络地址：https://betterbench.blog.csdn.net/article/details/109519282
+   卷积--标准化--池化
+        积输出维度计算已知：假设输入的图片 尺寸 ：A x A     卷积核 大小：K    步长Stride：S   Padding大小：P    求解： 输出的embedding 维度 B：B = (A + 2*P - K) / S + 1
+        
+        tf.keras.layers.Conv2D(
+        filters = 卷积核个数，
+        kernel_size = 卷积核尺寸，# 正方形写核长整数，或（核高h,核宽w）
+        strides = 滑动步长，#横纵向相同写步长整数，或（纵向步长h，横向步长w）,默认1
+        padding = “same” or “valid” .#使用全零填充是“same”，不适用是“valid”（默认）
+        activation = “relu” or “sigmoid” or “tanh” or "softmax"等，#如有BN（批标准化操作）此处不谢
+        input_shape = （高，宽，通道数）# 输入特征图维度，可省略)
 
+        model = tf.keras.models.Sequential([
+            Conv2D(filters = 6,kernel_size = (5,5),padding = 'same'),#卷积层
+            BatchNormalization(),#BN层
+            Activation（'relu'）,
+            MaxPool2D(pool_size =(2,2),strides = 2,padding = 'same'),
+            Dropout(0.2),# dropout层
+        ])
+
+
+        
